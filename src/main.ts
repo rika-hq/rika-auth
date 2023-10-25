97ddd743801f24500a85a65cbec261efcad9d644
Muhammed Karamuk <mkaramuk2002@gmail.com>
2023-10-25 20:26:38 +0300

    fix: Use port env variable

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { AppModule } from './app.module';
 import { config as envConfig } from 'dotenv';
 import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	envConfig();
 	const app = await NestFactory.createMicroservice(AppModule, {
 		transport: Transport.GRPC,
 		options: {
-			url: '0.0.0.0:5000',
+			url: `0.0.0.0:${process.env.PORT || 5000}`,
 			package: 'auth',
 			protoPath: join(__dirname, '../../proto/auth.proto'),
 		},
